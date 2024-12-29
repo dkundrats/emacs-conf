f693f76e0fa322281e81a29e312d11d05271144e
David Kundrats <david.kundrats@anikasystems.com>
2024-12-28 22:44:32 -0500

    other changes

--- init.el
+++ init.el
@@ -38,7 +38,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("00445e6f15d31e9afaa23ed0d765850e9cd5e929be5e8e63b114a3346236c44c" "4c56af497ddf0e30f65a7232a8ee21b3d62a8c332c6b268c81e9ea99b11da0d3" default))
+   '("00445e6f15d31e9afaa23ed0d765850e9cd5e929be5e8e63b114a3346236c44c"
+     "4c56af497ddf0e30f65a7232a8ee21b3d62a8c332c6b268c81e9ea99b11da0d3"
+     default))
  '(gdb-many-windows t)
  '(gdb-max-source-window-count 6)
  '(package-archives
@@ -47,9 +49,17 @@
      ("melpa" . "https://melpa.org/packages/")
      ("melpa-stable" . "https://stable.melpa.org/packages/")))
  '(package-selected-packages
-   '(gptel lsp-ui rustic flycheck-rust py-isort py-autopep8 zenburn-theme rust-mode eglot-booster lsp-pyright counsel-projectile counsel ivy ef-themes dap-mode typescript-mode pdf-tools page-break-lines nerd-icons dashboard all-the-icons elpy smartparens flycheck treemacs-tab-bar treemacs-persp treemacs-icons-dired treemacs-projectile treemacs-evil treemacs vterm-toggle vterm dape rainbow-identifiers olivetti ## magit use-package solarized-theme eglot))
+   '(## all-the-icons counsel counsel-projectile dap-mode dape dashboard
+	ef-themes eglot eglot-booster elpy flycheck flycheck-rust
+	geiser-mit gptel ivy lsp-pyright lsp-ui magit nerd-icons
+	olivetti page-break-lines pdf-tools py-autopep8 py-isort
+	rainbow-identifiers rust-mode rustic smartparens
+	solarized-theme treemacs treemacs-evil treemacs-icons-dired
+	treemacs-persp treemacs-projectile treemacs-tab-bar
+	typescript-mode use-package vterm vterm-toggle zenburn-theme))
  '(package-vc-selected-packages
-   '((eglot-booster :vc-backend Git :url "https://github.com/jdtsmith/eglot-booster")))
+   '((eglot-booster :vc-backend Git :url
+		    "https://github.com/jdtsmith/eglot-booster")))
  '(warning-suppress-log-types '((comp))))
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
